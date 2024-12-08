bb2a5fa24997e5116179ec1f2b156aa924759882
Артём Лазарев <artem.lazarev2006@gmail.com>
2024-12-08 23:05:11 +0500

    some changes

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@
 #include <termios.h>
 #include <unistd.h>
 #include <string>
+#include <vector>
 #include "modules/record.h"
 
 #define KEY_ARROW_UP 65
 #define KEY_ARROW_DOWN 66
 #define KEY_ENTER 10
+
+std::vector<Record> records;
 
 const std::string MENU[] = {
     "Загрузка из файла",
@@ -52,7 +55,7 @@ int main() {
         switch (key) {
             case KEY_ARROW_UP:
                 std::system("clear");
-                selected = selected > 1 ? --selected : selected = 11;
+                selected = selected > 1 ? --selected : selected = OPTION_COUNT;
                 break;
             case KEY_ARROW_DOWN:
                 std::system("clear");
@@ -63,11 +66,29 @@ int main() {
                 renderMenu(selected);
 
                 switch (selected) {
-                    case 1:
+                    case 1:  // Загрузка из файла
                         std::cout << "test";
                         break;
-                    case 11:
-                        return 0;
+                    case 2:  // Сохранение результатов обработки в файлах
+                        break;
+                    case 3:  // Добавление записи
+                        break;
+                    case 4:  // Удаление записи
+                        break;
+                    case 5:  // Вывод на экран дисплея данных и результатов обработки
+                        break;
+                    case 6:  // Алфавитная сортировка по фамилии
+                        break;
+                    case 7:  // Сортировка по названию марки автомобиля, а при совпадении марки — по фамилии
+                        break;
+                    case 8:  // Числовая сортировка по объему бака автомобиля
+                        break;
+                    case 9:  // Перечень марок автомобилей с указанием их числа (результат отсортирован по названию марки автомобиля в алфавитном порядке)
+                        break;
+                    case 10:  // Поиск по фамилии владельца (результат отсортирован по маркам в алфавитном порядке)
+                        break;
+                    case 11:  // Завершить
+                        isRunning = false;
                 }
                 continue;
             default:
